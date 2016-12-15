9bf8189bd80b05e3c620aa6032cc7a2801b7acd1
Kaynen <kaynenh@gmail.com>
2016-12-15 07:19:16 -0800

    Totals added to final column

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -16,8 +16,8 @@ import SpreadsheetComponent from 'react-spreadsheet-component';
 var exampleTwo = {};
 exampleTwo.initialData = {
   rows: [
-    ['Name', 'Hole 1', 'Hole 2', 'Hole 3', 'Hole 4', 'Hole 5', 'Hole 6', 'Hole 7', 'Hole 8', 'Hole 9'],
-    ['', '', '', '', '', '', '', '', '', '', ]
+    ['Name', 'Hole 1', 'Hole 2', 'Hole 3', 'Hole 4', 'Hole 5', 'Hole 6', 'Hole 7', 'Hole 8', 'Hole 9', 'Total'],
+    ['', '', '', '', '', '', '', '', '', '','' ]
   ]
 };
 
@@ -45,9 +45,16 @@ var Dispatcher = require('../../../node_modules/react-spreadsheet-component/lib/
 
 Dispatcher.subscribe('dataChanged', function (data) {
   // data: The entire new data state
+  console.log(data);
   var row1 = data.rows[1];
+  var sum = 0;
+  for (var i = 1; i<10; i++) {
+    sum += Number(data.rows[1][i]);
+  }
   console.log(row1);
-  var sum = row1.reduce((a, b) => (Number(a)>0?Number(a):0) + (Number(b)>0?Number(b):0), 0);
+  //var sum = row1.reduce((a, b) => (Number(a)>0?Number(a):0) + (Number(b)>0?Number(b):0), 0);
+
+  data.rows[1][10] = sum;
   console.log(sum);
 }, 'mainSheet');
 
